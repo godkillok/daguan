af37b4b611ef772e8c2c776f9e516c88a7f0bc26
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2018-08-21 21:09:16 +0800

    11

--- text/generate_csv_tfrecords.py
+++ text/generate_csv_tfrecords.py
@@ -103,6 +103,7 @@ def main():
     for root, dirs, files in os.walk(s3_input):
         for file in files:
             if file.endswith(".csv"):
+                print('start to process file {}'.format(file))
                 generate_tfrecords(os.path.join(root, file))
 
 
