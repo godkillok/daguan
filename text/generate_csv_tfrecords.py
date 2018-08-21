dfe9926f52574130016a701fd1f65c488a7cdf6d
godkillok <fujianhaowawa@163.com>
2018-08-21 08:42:30 +0800

    11

--- text/generate_csv_tfrecords.py
+++ text/generate_csv_tfrecords.py
@@ -5,20 +5,24 @@ import os
 import csv
 import re
 import numpy as np
+
 # 将数据转化成对应的属性
-sentence_max_len=100
-pad_word='<pad>'
+sentence_max_len = 100
+pad_word = '<pad>'
+
+
 def feature_auto(value):
-  if isinstance(value,int):
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+    if isinstance(value, int):
+        return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
-  elif isinstance(value,str):
+    elif isinstance(value, str):
 
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value.encode()]))
+        return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value.encode()]))
 
-  elif isinstance(value, float):
-    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+    elif isinstance(value, float):
+        return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
 
 def parse_line(line, vocab):
     def get_content(record):
@@ -43,40 +47,46 @@ def parse_line(line, vocab):
     result[1].set_shape([])
     # Lookup tokens to return their ids
     ids = vocab.lookup(result[0])
+    tf.tables_initializer().run()
+    ids.eval()
     return {"sentence": ids}, result[1] - 1
-def gete():12
-    vocab = tf.contrib.lookup.index_table_from_file(path_vocab, num_oov_buckets=num_oov_buckets)
+
+
+def gete():
+    path_vocab = os.path.join('./text.csv')
+    vocab = tf.contrib.lookup.index_table_from_file(path_vocab, num_oov_buckets=1)
+
 
 def generate_tfrecords(input_filename, output_filename):
-  print("Start to convert {} to {}".format(input_filename, output_filename))
-  writer = tf.python_io.TFRecordWriter(output_filename)
-  cout=0
-  with open(input_filename, 'r+', newline='') as csv_file:
-    reader = csv.reader(csv_file)
-    for data in reader:
-      text = str(data[0])
-      label = str(data[1])
+    print("Start to convert {} to {}".format(input_filename, output_filename))
+    writer = tf.python_io.TFRecordWriter(output_filename)
+    cout = 0
+    with open(input_filename, 'r+', newline='') as csv_file:
+        reader = csv.reader(csv_file)
+        for data in reader:
+            text = str(data[0])
+            label = str(data[1])
 
-      # To show an example of embedding
+            # To show an example of embedding
 
-      example = tf.train.Example(features=tf.train.Features(feature={
-        'text': feature_auto(text),
-        'label': feature_auto(label)
-      }))
-      if cout < 8:
-        writer.write(example.SerializeToString())
+            example = tf.train.Example(features=tf.train.Features(feature={
+                'text': feature_auto(text),
+                'label': feature_auto(label)
+            }))
+            if cout < 8:
+                writer.write(example.SerializeToString())
 
-  writer.close()
-  print("Successfully convert {} to {}".format(input_filename,
-                                               output_filename))
+    writer.close()
+    print("Successfully convert {} to {}".format(input_filename,
+                                                 output_filename))
 
 
 def main():
-  current_path = os.getcwd()
-  for filename in os.listdir(current_path):
-    if filename.startswith("") and filename.endswith(".csv"):
-      generate_tfrecords(filename, filename + ".tfrecords")
+    current_path = os.getcwd()
+    for filename in os.listdir(current_path):
+        if filename.startswith("") and filename.endswith(".csv"):
+            generate_tfrecords(filename, filename + ".tfrecords")
 
 
 if __name__ == "__main__":
-  main()
+    main()
