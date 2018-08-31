05ff4244517db447160dc9f03d3623bc5ed4919a
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2018-08-31 19:40:16 +0800

    11

--- base_line_svm.py
+++ base_line_svm.py
@@ -3,8 +3,21 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn import svm
 #12813123
 column = "word_seg"
+new_ = pd.read_csv('./cnn/valid_id')
 train = pd.read_csv('../input_data/train.csv')
 test = pd.read_csv('../input_data/test.csv')
+
+
+new_=pd.merge(new_, test, how='inner', on=['id', 'id'])
+print('merge_before')
+print(train._info_axis)
+print(train.shape)
+train = train.append(new_)
+print('merge_after')
+print(train._info_axis)
+print(train.shape)
+
+
 test_id = test["id"].copy()
 vec = TfidfVectorizer(ngram_range=(1,2),min_df=3, max_df=0.9,use_idf=1,smooth_idf=1, sublinear_tf=1)
 trn_term_doc = vec.fit_transform(train[column])
--- cnn/new_data.py
+++ cnn/new_data.py
@@ -3,7 +3,10 @@ import pandas as pd
 test = pd.read_csv('C:/work/input_data/test.csv')
 new_ = pd.read_csv('valid_id')
 # ge=test.join(new_, how ='inner',on='id')
-geg=pd.merge(new_, test, how='inner', on=['id', 'id'])
+new_=pd.merge(new_, test, how='inner', on=['id', 'id'])
+
 train = pd.read_csv('C:/work/input_data/train.csv')
+print(train._info_axis)
+print(train.shape)
 result = train.append(geg)
 print('gg')
