b474d0a40797263ea2391848d2755e14231826e7
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2018-08-31 19:34:07 +0800

    11

--- cnn/new_data.py
+++ cnn/new_data.py
@@ -1,5 +1,9 @@
 import csv
 import pandas as pd
-# train = pd.read_csv('../input_data/train.csv')
-train = pd.read_csv('valid_id')
-print(train)
+test = pd.read_csv('C:/work/input_data/test.csv')
+new_ = pd.read_csv('valid_id')
+# ge=test.join(new_, how ='inner',on='id')
+geg=pd.merge(new_, test, how='inner', on=['id', 'id'])
+train = pd.read_csv('C:/work/input_data/train.csv')
+result = train.append(geg)
+print('gg')
