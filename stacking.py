a9b65977b2b0a7fb68843bd1ea14c28bed6d54aa
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2018-09-03 19:58:28 +0800

    11

--- stacking.py
+++ stacking.py
@@ -47,10 +47,11 @@ dataset = Dataset(X_train,y_train,X_test,use_cache=False)
 model_nb = Classifier(dataset=dataset, estimator=MultinomialNB,name='nb',use_cache=False)
 model_lr = Classifier(dataset=dataset, estimator=LogisticRegression, parameters={'C':4, 'dual':True,'n_jobs':-1},name='lr',use_cache=False)
 model_svm = Classifier(dataset=dataset, estimator=svm.SVC, parameters={ 'probability':True},name='svm',use_cache=False)
-# model_svc= Classifier(dataset=dataset, estimator=svm.LinearSVC,name='LinearSVC')
+model_svc= Classifier(dataset=dataset, estimator=svm.LinearSVC,name='LinearSVC')
+
 # Stack两个模型mhg
 # Returns new dataset with out-of-fold prediction,model_svm,model_per
-pipeline = ModelsPipeline(model_svm,model_nb,model_lr)
+pipeline = ModelsPipeline(model_svc,model_nb,model_lr)
 stack_ds = pipeline.stack(k=10,seed=111)
 #第二层使用lr模型stack1
 stacker = Classifier(dataset=stack_ds, estimator=LogisticRegression,use_cache=False,probability=False)
