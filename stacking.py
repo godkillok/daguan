79141b816158439377b69f57310b1b48cf4d883c
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2018-09-04 16:51:08 +0800

    11

--- stacking.py
+++ stacking.py
@@ -87,7 +87,7 @@ model_gbdt=Classifier(dataset=dataset, estimator=GradientBoostingClassifier,name
 # Stack两个模型mhg
 # Returns new dataset with out-of-fold prediction,model_svm,model_per
 logging.info('stack_ds....')
-pipeline = ModelsPipeline(svm.SVC,model_lr)
+pipeline = ModelsPipeline(model_svc,model_lr)
 # pipeline = ModelsPipeline(model_nb),model_nb,model_lr,model_lr2
 stack_ds = pipeline.stack(k=5,seed=111)
 #第二层使用lr模型stack2
